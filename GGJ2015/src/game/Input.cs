eb8c819c07e4a96d3fa8dbbe64b0e93d35590c42
Karclan <radier206@hotmail.co.uk>
2015-01-24 13:19:52 +0000

    Input singleton
    HAHAHAHA SINGLETONS HAHAHAHA SINGLETONSHAHAHAHA SINGLETONS HAHAHAHA
    SINGLETONSHAHAHAHA SINGLETONS HAHAHAHA SINGLETONSHAHAHAHA SINGLETONS
    HAHAHAHA SINGLETONSHAHAHAHA SINGLETONS HAHAHAHA SINGLETONSHAHAHAHA
    SINGLETONS HAHAHAHA SINGLETONSHAHAHAHA SINGLETONS HAHAHAHA
    SINGLETONSHAHAHAHA SINGLETONS HAHAHAHA SINGLETONSHAHAHAHA SINGLETONS
    HAHAHAHA SINGLETONS

---
+++ GGJ2015/src/game/Input.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+
+class Input
+{
+    private HashSet<Keyboard.Key> _hassleHoff = new HashSet<Keyboard.Key>();
+
+    static Input _inst;
+    static Input inst { get { if (_inst == null) _inst = new Input(); return _inst; } }
+
+    private Input()
+    {
+    }
+
+    public static bool getKey(Keyboard.Key i)
+    {
+        foreach (Keyboard.Key k in inst._hassleHoff)
+        {
+            if(i==k)
+                return true; 
+        }
+        return false;
+    }
+    public static void keyPressed(Keyboard.Key i)
+    {
+        inst._hassleHoff.Add(i);
+    }
+    public static void keyReleased(Keyboard.Key i)
+    {
+            if (inst._hassleHoff.Contains(i)==true)
+            {
+                inst._hassleHoff.Remove(i);
+            }
+    }
+
+}
+
