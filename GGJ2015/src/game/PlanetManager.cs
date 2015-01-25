96f88c47d56e40bf3907b10119ef58d132b96836
Aleckz <p03069522@myemail.dmu.ac.uk>
2015-01-25 12:54:29 +0000

    Planets come continuouslyyy

--- GGJ2015/src/game/PlanetManager.cs
+++ GGJ2015/src/game/PlanetManager.cs
@@ -8,28 +8,43 @@ using SFML.Window;
 
 class PlanetManager
 {
+    int _currentPlanet = 0;
     Planet[] _planets = new Planet[3];
-   // Texture[] _textures = new Texture[2];
+    float _planetFreq = 30;
+    float _timeMeBro;
 
     public Planet[] planets { get { return _planets; } }
 
     public PlanetManager()
     {
-        Random random = new Random();
-        
-     //   _textures[0] = Assets.GetTexture("../../images/HassleMoon.png");
-      //  _textures[1] = Assets.GetTexture("../../images/LionelMoon.png");
-
         // Planet Implementation
-        _planets[0] = new Planet(random, Assets.GetTexture("../../images/HassleMoon.png"));
-        _planets[1] = new Planet(random, Assets.GetTexture("../../images/LionelMoon.png"));
-        _planets[2] = new Planet(random, Assets.GetTexture("../../images/HassleMoon.png"));
+        _planets[0] = new Planet(Assets.GetTexture("../../images/HassleMoon.png"));
+        _planets[1] = new Planet(Assets.GetTexture("../../images/LionelMoon.png"));
+        _planets[2] = new Planet(Assets.GetTexture("../../images/HassleMoon.png"));
+
+        _timeMeBro = 1;
     }
 
 
     public void Update()
     {
         foreach (Planet planet in _planets) planet.Update();
+
+        _timeMeBro -= Time.deltaTime;
+        if (_timeMeBro <= 0)
+        {
+            MakePlanet();
+            _timeMeBro = _planetFreq;
+        }
+    }
+
+    private void MakePlanet()
+    {
+        if (_planets[_currentPlanet].isActive) return;
+        _planets[_currentPlanet].SetActive(true);
+        _planets[_currentPlanet].Reset();
+        _currentPlanet += 1;
+        if(_currentPlanet >= planets.Length) _currentPlanet = 0;
     }
 
     public void DrawPlanets(RenderWindow window)
